ad5c3fd0af5c7b6586da1f9a50a1bb864dc2c280
zhangyi1015 <zycool0828@163.com>
2020-04-18 18:58:45 +0800

    around标签

--- spring_study4/src/main/java/com/zhy/util/LogUtil.java
+++ spring_study4/src/main/java/com/zhy/util/LogUtil.java
@@ -1,6 +1,7 @@
 package com.zhy.util;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +21,31 @@ public class LogUtil {
 
     @Pointcut("execution( public int com.zhy.inter.MyCalculator.*(int,int))")
     public void myPoint(){}
+
+
+
+    /**
+     * 环绕通知是spring中功能最强大的通知
+     * @param proceedingJoinPoint
+     * @return
+     */
+    @Around("myPoint()")
+    public Object myAround(ProceedingJoinPoint proceedingJoinPoint){
+        Object[] args = proceedingJoinPoint.getArgs();
+        String name = proceedingJoinPoint.getSignature().getName();
+        Object proceed = null;
+        try {
+            System.out.println("环绕前置通知:"+name+"方法开始，参数是"+Arrays.asList(args));
+            //利用反射调用目标方法，就是method.invoke()
+            proceed = proceedingJoinPoint.proceed(args);
+            System.out.println("环绕返回通知:"+name+"方法返回，返回值是"+proceed);
+        } catch (Throwable e) {
+            System.out.println("环绕异常通知"+name+"方法出现异常，异常信息是："+e);
+        }finally {
+            System.out.println("环绕后置通知"+name+"方法结束");
+        }
+        return proceed;
+    }
 
     /*
    设置下面方法在什么时候运行
